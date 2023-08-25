4218092f08929541ad05b179b723b416c538d4e9
RobertKarapetyan <robkarapet@gmail.com>
2023-08-25 10:40:17 -0700

    ShannonSecureAES

---
+++ SimpleCryptoLib.Tests/ShannonSecureAESTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCryptoLib.Tests
+{
+    [TestClass]
+    public class ShannonSecureAESTests
+    {
+        private ShannonSecureAES _aesHelper;
+        private string _testKey;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _aesHelper = new ShannonSecureAES();
+            _testKey = "SuperSecretTestKey1234SuperSecretTestKey1234"; 
+        }
+
+        [TestMethod]
+        public void TestDecryptWithWrongKeyFails()
+        {
+            string plainText = "Hello, Shannon!";
+
+            byte[] cipherText = _aesHelper.Encrypt(plainText, _testKey);
+            Assert.IsNotNull(cipherText);
+
+            string wrongKey = "WrongKey1234WrongKey1234WrongKe"; 
+
+            Assert.ThrowsException<CryptographicException>(() => _aesHelper.Decrypt(cipherText, wrongKey));
+        }
+
+
+        [TestMethod]
+        public void TestEncryptionDecryptionCycle()
+        {
+            string plainText = "Hello, Shannon!";
+
+            byte[] cipherText = _aesHelper.Encrypt(plainText, _testKey);
+            Assert.IsNotNull(cipherText);
+
+            string decryptedText = _aesHelper.Decrypt(cipherText, _testKey);
+            Assert.AreEqual(plainText, decryptedText);
+        }
+    }
+
+}
---
+++ SimpleCryptoLib/ShannonSecureAES.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SimpleCryptoLib
+{
+    public class ShannonSecureAES
+    {
+        private static readonly int keySize = 256; // in bits
+        private static readonly int blockSize = 128; // in bits
+
+        // Encrypt using AES
+        public byte[] Encrypt(string plainText, string key)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = keySize;
+                aes.BlockSize = blockSize;
+                aes.Key = GetKeyBytes(key);
+                aes.IV = new byte[blockSize / 8]; 
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                    cryptoStream.Write(plainBytes, 0, plainBytes.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+
+        // Decrypt using AES
+        public string Decrypt(byte[] cipherBytes, string key)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = keySize;
+                aes.BlockSize = blockSize;
+                aes.Key = GetKeyBytes(key);
+                aes.IV = new byte[blockSize / 8];
+
+                using (MemoryStream memoryStream = new MemoryStream(cipherBytes))
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                using (StreamReader streamReader = new StreamReader(cryptoStream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+
+        private byte[] GetKeyBytes(string key)
+        {
+            // Ensuring key is exactly 256 bits or 32 bytes
+            if (key.Length > 32)
+                return Encoding.UTF8.GetBytes(key.Substring(0, 32));
+            else
+                return Encoding.UTF8.GetBytes(key.PadRight(32, '\0'));
+        }
+    }
+}
