dcb75e0a47be8e7b2bd35de4033593ff93098e5c
RobertKarapetyan <robkarapet@gmail.com>
2023-08-24 19:33:17 -0700

    OneTimePad

--- SimpleCryptoLib.Tests/OneTimePadAttackTests.cs
+++ SimpleCryptoLib.Tests/OneTimePadAttackTests.cs
@@ -6,19 +6,58 @@ namespace SimpleCryptoLib.Tests
     public class OneTimePadAttackTests
     {
         [TestMethod]
-        public void TestRevealMessageAttack()
+        public void TestSamePredictableAndTargetMessage()
         {
-            string predictableMessage = "STATUS";
-            string confidentialMessage = "SECRET";
+            string predictableMessage = "STATUS: OK.";
+            string targetMessage = "STATUS: OK.";
 
             byte[] key = OneTimePad.GenerateRandomKey(Encoding.UTF8.GetBytes(predictableMessage).Length);
 
             byte[] predictableCiphertext = OneTimePad.Encrypt(Encoding.UTF8.GetBytes(predictableMessage), key);
-            byte[] targetCiphertext = OneTimePad.Encrypt(Encoding.UTF8.GetBytes(confidentialMessage), key);
+            byte[] targetCiphertext = OneTimePad.Encrypt(Encoding.UTF8.GetBytes(targetMessage), key);
 
             string revealedMessage = OneTimePadAttack.RevealMessageUsingPredictableMessage(predictableCiphertext, targetCiphertext, predictableMessage);
 
-            Assert.AreEqual(confidentialMessage, revealedMessage);
+            Assert.AreEqual(predictableMessage, revealedMessage);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDifferentLengths()
+        {
+            string predictableMessage = "STATUS: OK.";
+            string targetMessage = "STATUS: NOT OK.";
+
+            byte[] key = OneTimePad.GenerateRandomKey(Encoding.UTF8.GetBytes(predictableMessage).Length);
+
+            byte[] predictableCiphertext = OneTimePad.Encrypt(Encoding.UTF8.GetBytes(predictableMessage), key);
+            byte[] targetCiphertext = OneTimePad.Encrypt(Encoding.UTF8.GetBytes(targetMessage), key);
+
+            OneTimePadAttack.RevealMessageUsingPredictableMessage(predictableCiphertext, targetCiphertext, predictableMessage);
+        }
+
+        [TestMethod]
+        public void TestDifferentMessages()
+        {
+            string predictableMessage = "STATUS: OK.";
+            string targetMessage = "SECRET DATA.";
+
+            int maxLength = Math.Max(predictableMessage.Length, targetMessage.Length);
+
+            byte[] key = OneTimePad.GenerateRandomKey(maxLength);
+
+            if (predictableMessage.Length < maxLength)
+                predictableMessage = predictableMessage.PadRight(maxLength);
+            if (targetMessage.Length < maxLength)
+                targetMessage = targetMessage.PadRight(maxLength);
+
+            byte[] predictableCiphertext = OneTimePad.Encrypt(Encoding.UTF8.GetBytes(predictableMessage), key);
+            byte[] targetCiphertext = OneTimePad.Encrypt(Encoding.UTF8.GetBytes(targetMessage), key);
+
+            string revealedMessage = OneTimePadAttack.RevealMessageUsingPredictableMessage(predictableCiphertext, targetCiphertext, predictableMessage);
+
+            Assert.AreEqual(targetMessage, revealedMessage);
+        }
+
     }
 }
