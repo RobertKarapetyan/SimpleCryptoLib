0bdd862f8b61837fbc810dd03bc9798f03361c7e
RobertKarapetyan <robkarapet@gmail.com>
2023-08-25 19:54:09 -0700

    MultiplicativeCipher

---
+++ SimpleCryptoLib.Tests/MultiplicativeCipherTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace SimpleCryptoLib.Tests
+{
+    [TestClass]
+    public class MultiplicativeCipherTests
+    {
+        [TestMethod]
+        public void TestEncryption()
+        {
+            MultiplicativeCipher cipher = new MultiplicativeCipher();
+
+            string plaintext = "HELLO";
+            string expectedCiphertext = "VMHHQ";
+            string actualCiphertext = cipher.Encrypt(plaintext);
+
+            Assert.AreEqual(expectedCiphertext, actualCiphertext);
+        }
+
+        [TestMethod]
+        public void TestDecryption()
+        {
+            MultiplicativeCipher cipher = new MultiplicativeCipher();
+
+            string ciphertext = "VMHHQ";
+            string expectedPlaintext = "HELLO";
+            string actualPlaintext = cipher.Decrypt(ciphertext);
+
+            Assert.AreEqual(expectedPlaintext, actualPlaintext);
+        }
+    }
+}
+
---
+++ SimpleCryptoLib/MultiplicativeCipher.cs
@@ -0,0 +1,52 @@
+﻿namespace SimpleCryptoLib
+{
+    public class MultiplicativeCipher
+    {
+        private const int MODULUS = 26;
+        private const int KEY = 3; 
+        private const int INVERSE_KEY = 9;  
+
+        public string Encrypt(string plaintext)
+        {
+            string result = "";
+
+            foreach (char letter in plaintext.ToUpper())
+            {
+                if (Char.IsLetter(letter))
+                {
+                    int x = letter - 'A';
+                    int encryptedCharValue = (x * KEY) % MODULUS;
+                    result += (char)(encryptedCharValue + 'A');
+                }
+                else
+                {
+                    result += letter;
+                }
+            }
+
+            return result;
+        }
+
+        public string Decrypt(string ciphertext)
+        {
+            string result = "";
+
+            foreach (char letter in ciphertext.ToUpper())
+            {
+                if (Char.IsLetter(letter))
+                {
+                    int x = letter - 'A';
+                    int decryptedCharValue = (x * INVERSE_KEY) % MODULUS;
+                    result += (char)(decryptedCharValue + 'A');
+                }
+                else
+                {
+                    result += letter;
+                }
+            }
+
+            return result;
+        }
+    }
+}
+
