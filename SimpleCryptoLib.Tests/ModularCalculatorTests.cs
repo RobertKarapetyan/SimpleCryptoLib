53c14584621c4aac594b8796a41e9cfe7e003f3a
RobertKarapetyan <robkarapet@gmail.com>
2023-08-25 18:56:08 -0700

    ModularCalculator

---
+++ SimpleCryptoLib.Tests/ModularCalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿namespace SimpleCryptoLib.Tests
+{
+    [TestClass]
+    public class ModularCalculatorTests
+    {
+        [TestMethod]
+        public void TestModExp_3Power8_Mod7()
+        {
+            long result = ModularCalculator.ModExp(3, 8, 7);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestModExp_2Power10_Mod8()
+        {
+            long result = ModularCalculator.ModExp(2, 10, 8);
+            Assert.AreEqual(0, result); // As 2^10 = 1024, and 1024 mod 8 = 0
+        }
+
+        [TestMethod]
+        public void TestModExp_4Power3_Mod9()
+        {
+            long result = ModularCalculator.ModExp(4, 3, 9);
+            Assert.AreEqual(1, result); // As 4^3 = 64, and 64 mod 9 = 1
+        }
+
+        [TestMethod]
+        public void TestModExp_10Power0_Mod11()
+        {
+            long result = ModularCalculator.ModExp(10, 0, 11);
+            Assert.AreEqual(1, result); // Anything raised to the power 0 is 1.
+        }
+
+        [TestMethod]
+        public void TestModExp_5Power3_Mod5()
+        {
+            long result = ModularCalculator.ModExp(5, 3, 5);
+            Assert.AreEqual(0, result); // As 5^3 = 125, and 125 mod 5 = 0
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestModExp_InvalidModulus_ThrowsException()
+        {
+            ModularCalculator.ModExp(5, 3, 0);
+        }
+    }
+}
+
---
+++ SimpleCryptoLib/ModularCalculator.cs
@@ -0,0 +1,34 @@
+﻿namespace SimpleCryptoLib
+{
+    public static class ModularCalculator
+    {
+        public static long ModExp(long baseValue, long exp, long mod)
+        {
+            if (mod == 0)
+                throw new ArgumentException("The modulus (mod) cannot be zero.");
+
+            long result = 1;
+            while (exp > 0)
+            {
+                if (exp % 2 == 1)
+                    result = ReduceByMod(result * baseValue, mod);
+
+                baseValue = ReduceByMod(baseValue * baseValue, mod);
+                exp /= 2;
+            }
+
+            return result;
+        }
+
+        private static long ReduceByMod(long value, long mod)
+        {
+            while (value >= mod)
+            {
+                value -= mod;
+            }
+
+            return value;
+        }
+    }
+}
+
